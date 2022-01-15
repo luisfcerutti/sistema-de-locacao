136fb1e588e838923a999392f654f9677f951f65
Luis Cerutti <luis.f.cerutti@gmail.com>
2022-01-15 09:26:17 -0800

    Configuracao inicial rotas

---
+++ src/routers/RouterPrincipal.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+    Outlet
+} from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import Inicio from "../screens/inicio/Inicio";
+import Login from "../screens/login/Login";
+
+function DesconectarNecessario({ children }) {
+
+    const usuario = useSelector((state) => state.usuario)
+
+    return !usuario.autenticado ? children : <Navigate to="/" />;
+}
+
+function AcessoNecessario({ children }) {
+
+    const usuario = useSelector((state) => state.usuario)
+
+    return usuario.autenticado ? children : <Navigate to="/login" />;
+}
+
+export default function RouterPrincipal() {
+
+    return (
+        <Router>
+            <Routes
+            >
+                <Route path="/login" element={
+                    <DesconectarNecessario>
+                        <Login />
+                    </DesconectarNecessario>
+                } />
+                <Route path="/" element={
+                    <AcessoNecessario>
+                        <Outlet />
+                    </AcessoNecessario>
+                }>
+                    <Route path="" element={<Inicio />} />                    
+                </Route>
+                <Route path={"*"} element={
+                  <Navigate to="/" />
+                } />
+            </Routes>
+        </Router>
+    );
+}
