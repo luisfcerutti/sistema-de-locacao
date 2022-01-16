ed5a0c29bbe68e284a6d4096c22dac5f4fcddaf1
Luis Cerutti <luis.f.cerutti@gmail.com>
2022-01-15 16:11:19 -0800

    Criacao tela de login

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,7 +1,95 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { Button, Input, Typography, Form } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function Login(){
+import './Login.css'
+import { acessar } from '../../store/actions/usuario';
+import Loading from '../../common/Loading';
+
+const { Title } = Typography;
+
+export default function Login() {
+
+    const dispatch = useDispatch()
+    const usuario = useSelector((state) => state.usuario)
+    const mounted = useRef(false)
+
+    const [usuarioAcesso, setUsuarioAcesso] = useState('')
+    const [senha, setSenha] = useState('')
+
+    const [erroUsuarioAcesso, setErroUsuarioAcesso] = useState('')
+    const [erroSenha, setErroSenha] = useState('')
+
+    useEffect(() => {
+        mounted.current = true
+
+        return () => {
+            mounted.current = false
+        }
+    }, [])
+
+    const onChangeUsuarioAcesso = (event) => {
+        setUsuarioAcesso(event.target.value)
+    }
+
+    const onChangeSenha = (event) => {
+        setSenha(event.target.value)
+    }
+
+    const limpaErros = () => {
+        if(mounted.current){
+            setErroUsuarioAcesso('')
+            setErroSenha('')
+        }
+    }
+
+    const clicaAcessar = () => {
+        limpaErros()
+
+        let usuarioOk = validarUsuario(usuarioAcesso)
+        let senhaOk = validarSenha(senha)
+
+        if(senhaOk && usuarioOk){
+            dispatch(acessar({ usuario: usuarioAcesso, senha: senha }))
+        }else{
+            if(!senhaOk){
+                setErroSenha('Senha curta')
+            }
+            if(!usuarioOk){
+                setErroUsuarioAcesso('Insira um usuário válido')
+            }
+
+            setTimeout(() => {
+                limpaErros()
+            }, 3000)
+        }
+    }
+
     return (
-        <h1>Login</h1>
+        <div className='div-principal'>
+            <div className='div-logo'>
+                <Title level={3} className='title' style={{ color: 'black', textAlign: 'center' }}>Sistema Locação</Title>
+            </div>
+            <div className='div-form'>
+                <Title level={5} style={{ color: 'black', textAlign: 'center' }}>Acessar</Title>
+                <Form className='formulario' layout={'vertical'} name="formulario-acesso">
+                    <Form.Item name="usuarioAcesso" id='usuarioAcesso' label="Usuário" validateStatus={erroUsuarioAcesso.length > 0 ? 'error' : ''} help={erroUsuarioAcesso} rules={[{ required: true }]}>
+                        <Input id='usuario-input' name='usuario-input' value={usuarioAcesso} onChange={onChangeUsuarioAcesso} />
+                    </Form.Item>
+                    <Form.Item name="senha" id='senha' label="Senha" validateStatus={erroSenha.length > 0 ? 'error' : ''} help={erroSenha} rules={[{ required: true }]}>
+                        <Input.Password id='senha-input' name='senha-input' value={senha} onChange={onChangeSenha} />
+                    </Form.Item>                    
+                    <Form.Item wrapperCol={{ offset: 7 }}>
+                        <Button type="primary" shape='round' size='large' onClick={clicaAcessar} className='botao-acessar'>
+                            Acessar
+                        </Button>                       
+                    </Form.Item>
+                </Form>
+            </div>
+            {usuario.carregando && <Loading mensagem={'Realizando acesso...'}/>}
+        </div>
     )
 }
+
+export const validarUsuario = (dados = "") => dados.trim().length>=5
+export const validarSenha = (dados = "") => dados.trim().length>=6
---
+++ src/screens/login/Login.test.js
@@ -0,0 +1,21 @@
+import { validarSenha, validarUsuario } from './Login';
+
+describe('tela login', () => {   
+
+    test('funcao de validar usuário deve passar com tamanho correto', () => {
+        const texto = 'admin'
+        expect(validarUsuario(texto)).toBe(true)
+    })
+    test('funcao de validar usuário deve rejeitar com tamanho incorreto', () => {
+        const texto = 'user'
+        expect(validarUsuario(texto)).not.toBe(true)
+    })
+    test('funcao de validar senha deve passar com tamanho correto', () => {
+        const texto = '654321'
+        expect(validarSenha(texto)).toBe(true)
+    })
+    test('funcao de validar senha deve rejeitar com tamanho incorreto', () => {
+        const texto = '65432'
+        expect(validarSenha(texto)).not.toBe(true)
+    })    
+})
